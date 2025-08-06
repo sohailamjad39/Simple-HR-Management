3441914711b4d30282a6f086c173fa218ec9b603
sohailamjad39 <sohailamjad39sgd@gmail.com>
2025-08-06 15:47:46 +0500

    fix: document title and favicon

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
-import Navbar from "./components/Navbar";
 import "./App.css";
 
 function App() {
